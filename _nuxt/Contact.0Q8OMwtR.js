6bab2c54849c944e92bef119aa634eb56fee57c1
Batyr Akmyradov <akmrbat@gmail.com>
2024-01-16 13:37:40 +0100

    fix: new base url

--- _nuxt/Contact.0Q8OMwtR.js
+++
@@ -1,1 +0,0 @@
-import"./entry.s0cwc7CA.js";const r=""+new URL("Contact.SW2yr_zI.jpg",import.meta.url).href;export{r as _};
---
+++ _nuxt/Contact.tWyg7Jma.js
@@ -0,0 +1,1 @@
+import"./entry.k9sEeKJc.js";const r=""+new URL("Contact.SW2yr_zI.jpg",import.meta.url).href;export{r as _};
