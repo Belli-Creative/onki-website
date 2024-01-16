2e839e602ef3428a25e582d71bfd47c0197c70e3
Batyr Akmyradov <akmrbat@gmail.com>
2024-01-16 12:06:58 +0100

    inital commit

---
+++ _nuxt/Bead maker.ZzO1Wsf7.js
@@ -0,0 +1,1 @@
+import"./entry.BnbsHm8u.js";const e=""+new URL("Bead maker.Mi1u2ipN.jpg",import.meta.url).href;export{e as _};
---
+++ _nuxt/Contact.CVvEOVFw.js
@@ -0,0 +1,1 @@
+import"./entry.BnbsHm8u.js";const r=""+new URL("Contact.SW2yr_zI.jpg",import.meta.url).href;export{r as _};
