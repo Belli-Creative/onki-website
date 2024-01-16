17863b64f0b5579c98aa77a9b95dcee67285b123
Batyr Akmyradov <akmrbat@gmail.com>
2024-01-16 13:35:12 +0100

    fix: base path

--- _nuxt/Bead maker.8Uj_zPok.js
+++
@@ -1,1 +0,0 @@
-import"./entry.Z_ivG9Zg.js";const e=""+new URL("Bead maker.Mi1u2ipN.jpg",import.meta.url).href;export{e as _};
--- _nuxt/Contact.eEg4wlMm.js
+++
@@ -1,1 +0,0 @@
-import"./entry.Z_ivG9Zg.js";const r=""+new URL("Contact.SW2yr_zI.jpg",import.meta.url).href;export{r as _};
